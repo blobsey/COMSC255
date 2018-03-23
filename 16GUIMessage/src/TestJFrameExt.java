e2c795279cb75fc2462f11da8c7ba8c730eb4f53
blobsey <blobs03@gmail.com>
2018-03-23 16:57:19 -0700

    assignment 16 and 17

---
+++ 16GUIMessage/src/TestJFrameExt.java
@@ -0,0 +1,18 @@
+//Assignment #: 16
+
+//Student Name:  Blake Randall
+
+//Class:  COMSC-255
+
+//Section: 8306
+
+
+public class TestJFrameExt {
+	public static void main(String[] args) {
+		JFrameExt f = new JFrameExt();
+		
+		f.setSize(400, 300);
+		
+		f.setVisible(true);
+	}
+}
