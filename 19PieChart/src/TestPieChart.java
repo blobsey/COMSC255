c46ebf8e134d37f07269ba83fbe2c5917196e001
blobsey <blobs03@gmail.com>
2018-04-13 18:10:59 -0700

    19 boyz

---
+++ 19PieChart/src/TestPieChart.java
@@ -0,0 +1,20 @@
+//Assignment #: 19
+
+//Student Name:  Blake Randall
+
+ //Class:  COMSC-255
+
+//Section: 8306
+
+
+public class TestPieChart {
+	public static void main(String[] args)
+	{
+		PieChart pc = new PieChart(20,25,15,30,10);
+		pc.setSize(500, 500);
+		pc.setVisible(true);
+		pc.paint(null);
+		
+	}
+	
+}
